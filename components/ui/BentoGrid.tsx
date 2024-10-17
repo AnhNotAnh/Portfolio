52a0919848569cb3aa3417ee73257f4cb04c5717
Duy Quoc Anh Nguyen <boyporo2017@gmail.com>
2024-10-18 09:16:43 +1030

    Fix how card display
    Card num1 is displayed full col when zooming out, fix this help all cards lay out better instead of giving all spaces to card num1

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -18,7 +18,8 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
+        // change gap-4 to gap-8, change grid-cols-3 to grid-cols-5, remove md:auto-rows-[18rem], add responsive code
+        "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
         className
       )}
     >
@@ -77,7 +78,7 @@ export const BentoGridItem = ({
             </div>
             {id === 6 && (
                 <BackgroundGradientAnimation>
-                    <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl" />
+                    {/* <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl" /> */}
                 </BackgroundGradientAnimation>
             )}
             <div className={cn(
