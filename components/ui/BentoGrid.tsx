fd6b2cb0c8ab3075975fe43e1fb57bfd73804c8e
Duy Quoc Anh Nguyen <boyporo2017@gmail.com>
2025-03-07 13:49:51 +1030

    Update new email

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -51,7 +51,7 @@ export const BentoGridItem = ({
   const [copied, setCopied] = useState(false);
 
   const handleCopy = () => { 
-    navigator.clipboard.writeText('quocanh01082020@gmail.com');
+    navigator.clipboard.writeText('quocanhwork@outlook.com');
     setCopied(true);
   }
 
