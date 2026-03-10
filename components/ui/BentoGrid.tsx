dbedf386da48548973be7606066a9d4a59c2dae7
Duy Quoc Anh Nguyen <boyporo2017@gmail.com>
2026-03-10 20:43:20 +1030

    Upgrade package

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -4,10 +4,12 @@ import { BackgroundGradientAnimation } from "./GradientBg";
 import { GlobeDemo } from "./GridGlobe";
 import { useState } from "react";
 import animationData from '@/data/confetti.json'
-import Lottie from "react-lottie";
+import dynamic from "next/dynamic";
 import MagicButton from "./MagicButton";
 import { IoCopyOutline } from "react-icons/io5";
 import TimeLocation from "./TimeLocation";
+
+const Lottie = dynamic(() => import("react-lottie"), { ssr: false });
 
 export const BentoGrid = ({
   className,
